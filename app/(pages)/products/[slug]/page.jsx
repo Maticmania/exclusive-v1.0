ff93700e7bc88770de424361278513c3ab7c77f6
Matic.dev <olaitantoyeeb1@gmail.com>
2025-04-22 19:27:57 +0100

    fixing little bug

--- app/(pages)/products/[slug]/page.jsx
+++ app/(pages)/products/[slug]/page.jsx
@@ -5,8 +5,11 @@ import RelatedProducts from "@/components/products/related-products";
 
 // Helper function to get the full URL including the protocol
 function getBaseUrl() {
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  return process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  if (process.env.VERCEL_URL) {
+    // If deployed on Vercel, use the VERCEL_URL environment variable
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  return process.env.NEXT_PUBLIC_BASE_URL;
 }
 
 async function getProduct(slug) {
